98aea53baf439c0d38682378f7becd869aaa8d49
yensaki <mov.an.double@gmail.com>
2020-09-19 17:06:18 +0900

    Reword sentense

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+  <Layout title="Home | Yen Saki Ika">
+    <h1>Yen Saki Ika</h1>
     <p>
       <Link href="/about">
         <a>About</a>
